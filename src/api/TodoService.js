9fb0284125d1cad5c11607d1f5d36ccbf148320d
andrii-ostapov <167943AOR>
2022-12-09 15:20:08 +0200

    rename

--- src/api/TodoService.js
+++ src/api/TodoService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export default class PostService {
+export default class TodoService {
     static async getAll(limit = 10) {
         const response = await axios.get('https://jsonplaceholder.typicode.com/todos', {
             params: {
